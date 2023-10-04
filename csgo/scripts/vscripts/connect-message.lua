61490545f11caaf45c0b22370965823b88e3b151
ALEXELMANCO <71610773+subzzone@users.noreply.github.com>
2023-10-05 00:16:33 +0200

    Update connect-message.lua
    Info in README

--- csgo/scripts/vscripts/connect-message.lua
+++ csgo/scripts/vscripts/connect-message.lua
@@ -1,77 +1,83 @@
 --[[
 ******************************************************[ CONNECT MESSAGE CS2 ]********************************************************
-		Plugin created by: Alexelmanco
-		Github: https://github.com/subzzone
-		Discord: Alexelmanco#7184
-		Steam: https://steamcommunity.com/id/alexelmanco/
-		________________________________________________________________________
-		USABILITY:
-		- This plugin is a simple Connect Message to the server
-
+	Plugin created by: Alexelmanco
+	Github: https://github.com/subzzone
+	Discord: Alexelmanco#7184
+	Steam: https://steamcommunity.com/id/alexelmanco/
+	________________________________________________________________________
+	USABILITY:
+	- This plugin is a simple Connect Message to the server
 ************************************************************************************************************************************
 --]]
 
-print("CONNECT MESAGE ON")
+-- Imports
+local config = require("connect-message.cfg.cmconfig")
+local language = require("connect-message.cfg.cmlanguages")
 
--- Remove duplicated listeners 
+-- Simple print
+print("[CONNECT MESSAGE] THE SCRIPT IS ON")
+
+-- Remove duplicated listeners upon manual reload
 if tListenerIds then
     for k, v in ipairs(tListenerIds) do
         StopListeningToGameEvent(v)
     end
 end
 
--- Global variables --------------------
-local count = 1;
-local usersId = {} --User id array
+-- Global variables
+local count = 1
+local usersId = {} -- User id array
 local usersIsBot = {} -- User bool bot
 
+-- Function to handle player connection
 function OnPlayerPutInServer(event)
+    -- User Variables
+    local useridConnect = event.userid
+    local nameConnect = event.name
+    local isBot = event.bot
 
-	-- User Variables -------------------
-	local useridConnect = event.userid;
-	local nameConnect = event.name
-	local isBot = event.bot;
+    -- IsBot saver
+    usersId[count] = useridConnect
+    usersIsBot[count] = isBot
+    count = count + 1
 
-	-- IsBot saver --------------------
-	usersId[count] = useridConnect;
-	usersIsBot[count] = isBot;
-	count = count + 1
-
-	-- Print message --------------------
-	if (isBot == 1) then
-		ScriptPrintMessageChatAll("\x04 \x04 [Server] Bot \x08 \x08" .. nameConnect .. "\x04 \x04 connected.");
-	else
-		ScriptPrintMessageChatAll("\x04 \x04 [Server] User \x08 \x08" .. nameConnect .. "\x04 \x04 connected.");
-	end
-
+    -- Print message
+    if isBot == 1 then
+        if config.botNotification then
+            ScriptPrintMessageChatAll("\x04 \x04 [ " .. config.nameTag .. " ] " .. language[config.language][1] .. "\x08 \x08" .. nameConnect .. "\x04 \x04 " .. language[config.language][4])
+        end
+    else
+        ScriptPrintMessageChatAll("\x04 \x04 [ " .. config.nameTag .. " ] " .. language[config.language][2] .. "\x08 \x08" .. nameConnect .. "\x04 \x04 " .. language[config.language][4])
+    end
 end
 
+-- Function to handle player disconnection
 function OnPlayerDisconnect(event)
+    -- User Variables
+    local useridDisconnect = event.userid
+    local nameDisconnect = event.name
+    local isBot = 0
 
-	-- User Variables -------------------
-	local useridDisconnect = event.userid;
-	local nameDisconnect = event.name;
-	local isBot = 0;
+    -- Loop to find the user id and add isBot
+    for i = 1, #usersId do
+        if usersId[i] == useridDisconnect then
+            isBot = usersIsBot[i]
+            break
+        end
+    end
 
-	-- Loop to find the user id and add isBot--
-	for i = 1, #usersId do
-		if (usersId[i] == useridDisconnect) then
-			isBot = usersIsBot[i];
-			break
-		end
-	end
-
-	-- Print message --------------------
-	if (isBot == 1) then
-		ScriptPrintMessageChatAll("\x07 \x07 [Server] Bot \x08 \x08" .. nameDisconnect .. "\x07 \x07 disconnected.");
-	else
-		ScriptPrintMessageChatAll("\x07 \x07 [Server] User \x08 \x08" .. nameDisconnect .. "\x07 \x07 disconnected.");
-	end
-
+    -- Print message
+    if isBot == 1 then
+        if config.botNotification then
+            ScriptPrintMessageChatAll("\x07 \x07 [ " .. config.nameTag .. " ] " .. language[config.language][1] .. "\x08 \x08" .. nameDisconnect .. "\x07 \x07 " .. language[config.language][3])
+        end
+    else
+        ScriptPrintMessageChatAll("\x07 \x07 [ " .. config.nameTag .. " ] " .. language[config.language][2] .. "\x08 \x08" .. nameDisconnect .. "\x07 \x07 " .. language[config.language][3])
+    end
 end
 
--- tListenerIds --------------------
+-- tListenerIds
 tListenerIds = {
-	ListenToGameEvent("player_connect", OnPlayerPutInServer, nil),
-	ListenToGameEvent("player_disconnect", OnPlayerDisconnect, nil),
+    ListenToGameEvent("player_connect", OnPlayerPutInServer, nil),
+    ListenToGameEvent("player_disconnect", OnPlayerDisconnect, nil),
 }
